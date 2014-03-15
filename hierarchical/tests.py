a6bda08dc07217f972e15f8332b7eb5615938d4b
annoviko <spb.andr@yandex.ru>
2014-03-15 16:57:19 +0400

    Added unit-tests for hierarchical algorithm

---
+++ hierarchical/tests.py
@@ -0,0 +1,41 @@
+import unittest;
+
+from hierarchical import hierarchical;
+from support import read_sample;
+
+class Test(unittest.TestCase):
+    def templateClusteringResults(self, path, number_clusters, expected_length_clusters):
+        sample = read_sample(path);
+        clusters = hierarchical(sample, number_clusters);
+        
+        assert sum([len(cluster) for cluster in clusters]) == len(sample);
+        assert sum([len(cluster) for cluster in clusters]) == sum(expected_length_clusters);
+        assert sorted([len(cluster) for cluster in clusters]) == expected_length_clusters;
+    
+    def testClusteringSampleSimple1(self):
+        self.templateClusteringResults("../Samples/SampleSimple1.txt", 2, [5, 5]);
+        self.templateClusteringResults("../Samples/SampleSimple1.txt", 1, [10]);
+
+    def testClusteringSampleSimple2(self):
+        self.templateClusteringResults("../Samples/SampleSimple2.txt", 3, [5, 8, 10]);
+        self.templateClusteringResults("../Samples/SampleSimple2.txt", 1, [23]);
+
+    def testClusteringSampleSimple3(self):
+        self.templateClusteringResults("../Samples/SampleSimple3.txt", 4, [10, 10, 10, 30]);
+        self.templateClusteringResults("../Samples/SampleSimple3.txt", 1, [60]);
+        
+    def testClusteringSampleSimple4(self):
+        self.templateClusteringResults("../Samples/SampleSimple4.txt", 5, [15, 15, 15, 15, 15]);
+        self.templateClusteringResults("../Samples/SampleSimple4.txt", 1, [75]);
+        
+    def testClusteringSampleSimple5(self):
+        self.templateClusteringResults("../Samples/SampleSimple5.txt", 4, [15, 15, 15, 15]);
+        self.templateClusteringResults("../Samples/SampleSimple5.txt", 1, [60]);    
+        
+    def testClusteringHepta(self):
+        self.templateClusteringResults("../Samples/SampleHepta.txt", 7, [30, 30, 30, 30, 30, 30, 32]);
+        self.templateClusteringResults("../Samples/SampleHepta.txt", 1, [212]);    
+
+
+if __name__ == "__main__":
+    unittest.main();
