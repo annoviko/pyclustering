dc864a6372e1cb1df80593be19c07f5c1a704c36
annoviko <spb.andr@yandex.ru>
2014-03-19 23:33:58 +0400

    Added examples to module [nnet.sync] with negative connections

--- nnet/sync/examples.py
+++ nnet/sync/examples.py
@@ -48,11 +48,48 @@ def time_dependence_grid_struct():
         network = sync.net(num_osc, 1, type_conn = sync.conn_type.GRID_FOUR, initial_phases = sync.initial_type.EQUIPARTITION);
         (t, dyn_phase) = network.simulate(50, 10, solution = sync.solve_type.ODEINT);
         
+        
+def negative_connection_5_oscillators():
+    network = sync.net(5, -1);
+    (t, dyn_phase) = network.simulate_dynamic(collect_dynamic = True, solution = sync.solve_type.FAST);
+    sync.draw_dynamics(t, dyn_phase); 
+       
+    
+def negative_connection_10_oscillators():
+    "Comment: It is not full desynchronization"
+    network = sync.net(10, -5);
+    (t, dyn_phase) = network.simulate_dynamic(collect_dynamic = True, solution = sync.solve_type.FAST);
+    sync.draw_dynamics(t, dyn_phase);
+    
+    
+def negative_connection_9_grid_struct():
+    "Comment: Right coloring"
+    network = sync.net(9, -2, type_conn = sync.conn_type.GRID_FOUR);
+    (t, dyn_phase) = network.simulate_dynamic(collect_dynamic = True, solution = sync.solve_type.FAST);
+    sync.draw_dynamics(t, dyn_phase);
+    
+    print(network.allocate_sync_ensembles(0.1));
+    
+    
+def negative_connection_16_grid_struct():
+    "Comment: Wrong coloring"
+    network = sync.net(16, -3, type_conn = sync.conn_type.GRID_FOUR);
+    (t, dyn_phase) = network.simulate_dynamic(collect_dynamic = True, solution = sync.solve_type.FAST);
+    sync.draw_dynamics(t, dyn_phase);
+    
+    print(network.allocate_sync_ensembles(0.1));
     
 
+# Examples of global synchronization and local (via q parameter).
 trivial_dynamic_sync();
 weight_5_dynamic_sync();
 cluster_2_dynamic_sync();
 cluster_5_dynamic_sync();
 bidir_struct_dynamic_sync();
 grid_four_struct_dynamic_sync();
+
+# Examples with negative connection
+negative_connection_5_oscillators();        # Almost full desynchronization
+negative_connection_10_oscillators();       # It's not full desynchronization
+negative_connection_9_grid_struct();        # Right coloring
+negative_connection_16_grid_struct();       # Wrong coloring
