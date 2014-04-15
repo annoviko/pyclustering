7d6593fdf74cea47e75aaca205dd12c5a8b12905
U-NSN-INTRA\annoviko <annoviko@CNU310BD26.nsn-intra.net>
2014-04-15 17:19:26 +0400

    Unit-tests have been added for ROCK algorithm

---
+++ rock/tests.py
@@ -0,0 +1,50 @@
+import unittest;
+
+from rock import rock;
+
+from support import read_sample;
+
+from samples.definitions import SIMPLE_SAMPLES;
+
+class Test(unittest.TestCase):  
+    def templateLengthProcessData(self, path_to_file, radius, cluster_numbers, threshold, expected_cluster_length):
+        sample = read_sample(path_to_file);
+        
+        clusters = rock(sample, radius, cluster_numbers, threshold);
+        
+        length = sum([len(cluster) for cluster in clusters]);
+        assert len(sample) == length;
+        
+        obtained_cluster_sizes = [len(cluster) for cluster in clusters];
+        obtained_cluster_sizes.sort();
+        expected_cluster_length.sort();
+        
+        assert obtained_cluster_sizes == expected_cluster_length;
+
+
+    def testClusterAllocationSampleSimple1(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, 1, 2, 0.5, [5, 5]);
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, 5, 1, 0.5, [10]);
+        
+    def testClusterAllocationSampleSimple2(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, 1, 3, 0.5, [10, 5, 8]);
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, 5, 1, 0.5, [23]);
+        
+    def testClusterAllocationSampleSimple3(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, 1, 4, 0.5, [10, 10, 10, 30]);
+        
+    def testClusterAllocationSampleSimple3WrongRadius(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, 1.7, 4, 0.5, [10, 10, 10, 30]);
+        
+    def testClusterAllocationSampleSimple4(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE4, 1, 5, 0.5, [15, 15, 15, 15, 15]);    
+
+    def testClusterAllocationSampleSimple4WrongRadius(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE4, 1.5, 5, 0.5, [15, 15, 15, 15, 15]);  
+
+    def testClusterAllocationSampleSimple5(self):
+        self.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE5, 1, 4, 0.5, [15, 15, 15, 15]);    
+        
+
+if __name__ == "__main__":
+    unittest.main()
