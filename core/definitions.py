58ed0ee4aff9dd4729ee8ecd789ca18c95a40e37
annoviko <annoviko@example.com>
2014-06-30 16:40:11 +0400

    Added few comments

--- core/definitions.py
+++ core/definitions.py
@@ -9,12 +9,24 @@ PATH_DLL_CCORE_WIN64 = core.__path__[0] + "\\ccore\\x64\\ccore.dll";
 # Structures that are required for exchaging with DLL.
 class cluster_representation(Structure):
     "Decription of cluster in memory"
+    " - unsigned int number_objects"
+    " - unsigned int * cluster_representation"
+    
     _fields_ = [("number_objects", c_uint), ("pointer_objects", POINTER(c_uint))];
+    
     
 class clustering_result(Structure):
     "Description of result of clustering in memory"
+    " - unsigned int number_clusters"
+    " - cluster_representation * pointer_clusters"
+    
     _fields_ = [("number_clusters", c_uint), ("pointer_clusters", POINTER(cluster_representation))];
+
 
 class data_representation(Structure):
     "Description of input data"
+    " - unsigned int number_object"
+    " - double ** pointer_objects"
+    
     _fields_ = [("number_objects", c_uint), ("dimension", c_uint), ("pointer_objects", POINTER(POINTER(c_double)))];
+    
