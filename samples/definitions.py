f0ff0271434cba604e2dd894c95aba9673f2dd42
annoviko <spb.andr@yandex.ru>
2014-08-06 22:06:08 +0400

    Linux adaptation: path correction to samples

--- samples/definitions.py
+++ samples/definitions.py
@@ -1,55 +1,56 @@
 import samples;
+import os;
 
 class SIMPLE_SAMPLES:
-    SAMPLE_SIMPLE1          = samples.__path__[0] + "\\SampleSimple1.txt";
-    SAMPLE_SIMPLE2          = samples.__path__[0] + "\\SampleSimple2.txt";
-    SAMPLE_SIMPLE3          = samples.__path__[0] + "\\SampleSimple3.txt";
-    SAMPLE_SIMPLE4          = samples.__path__[0] + "\\SampleSimple4.txt";
-    SAMPLE_SIMPLE5          = samples.__path__[0] + "\\SampleSimple5.txt";
-    SAMPLE_ELONGATE         = samples.__path__[0] + "\\SampleElongate.txt";
+    SAMPLE_SIMPLE1          = samples.__path__[0] + os.sep + "SampleSimple1.txt";
+    SAMPLE_SIMPLE2          = samples.__path__[0] + os.sep + "SampleSimple2.txt";
+    SAMPLE_SIMPLE3          = samples.__path__[0] + os.sep + "SampleSimple3.txt";
+    SAMPLE_SIMPLE4          = samples.__path__[0] + os.sep + "SampleSimple4.txt";
+    SAMPLE_SIMPLE5          = samples.__path__[0] + os.sep + "SampleSimple5.txt";
+    SAMPLE_ELONGATE         = samples.__path__[0] + os.sep + "SampleElongate.txt";
 
 class FCPS_SAMPLES:
-    SAMPLE_ATOM             = samples.__path__[0] + "\\SampleAtom.txt";
-    SAMPLE_CHAINLINK        = samples.__path__[0] + "\\SampleChainlink.txt";
-    SAMPLE_ENGY_TIME        = samples.__path__[0] + "\\SampleEngyTime.txt";
-    SAMPLE_GOLF_BALL        = samples.__path__[0] + "\\SampleGolfBall.txt";
-    SAMPLE_HEPTA            = samples.__path__[0] + "\\SampleHepta.txt";
-    SAMPLE_LSUN             = samples.__path__[0] + "\\SampleLsun.txt";
-    SAMPLE_TARGET           = samples.__path__[0] + "\\SampleTarget.txt";
-    SAMPLE_TETRA            = samples.__path__[0] + "\\SampleTetra.txt";
-    SAMPLE_TWO_DIAMONDS     = samples.__path__[0] + "\\SampleTwoDiamonds.txt";
-    SAMPLE_WING_NUT         = samples.__path__[0] + "\\SampleWingNut.txt";
+    SAMPLE_ATOM             = samples.__path__[0] + os.sep + "SampleAtom.txt";
+    SAMPLE_CHAINLINK        = samples.__path__[0] + os.sep + "SampleChainlink.txt";
+    SAMPLE_ENGY_TIME        = samples.__path__[0] + os.sep + "SampleEngyTime.txt";
+    SAMPLE_GOLF_BALL        = samples.__path__[0] + os.sep + "SampleGolfBall.txt";
+    SAMPLE_HEPTA            = samples.__path__[0] + os.sep + "SampleHepta.txt";
+    SAMPLE_LSUN             = samples.__path__[0] + os.sep + "SampleLsun.txt";
+    SAMPLE_TARGET           = samples.__path__[0] + os.sep + "SampleTarget.txt";
+    SAMPLE_TETRA            = samples.__path__[0] + os.sep + "SampleTetra.txt";
+    SAMPLE_TWO_DIAMONDS     = samples.__path__[0] + os.sep + "SampleTwoDiamonds.txt";
+    SAMPLE_WING_NUT         = samples.__path__[0] + os.sep + "SampleWingNut.txt";
 
 class GRAPH_SIMPLE_SAMPLES:
-    GRAPH_BROKEN_CIRCLE1            = samples.__path__[0] + "\\graphs\\GraphBrokenCircle1.grpr";
-    GRAPH_BROKEN_CIRCLE2            = samples.__path__[0] + "\\graphs\\GraphBrokenCircle2.grpr";
-    GRAPH_FIVE_POINTED_FRAME_STAR   = samples.__path__[0] + "\\graphs\\GraphFivePointedFrameStar.grpr";
-    GRAPH_FIVE_POINTED_STAR         = samples.__path__[0] + "\\graphs\\GraphFivePointedStar.grpr";
-    GRAPH_ONE_CIRCLE1               = samples.__path__[0] + "\\graphs\\GraphOneCircle1.grpr";
-    GRAPH_ONE_CIRCLE2               = samples.__path__[0] + "\\graphs\\GraphOneCircle2.grpr";
-    GRAPH_ONE_CIRCLE3               = samples.__path__[0] + "\\graphs\\GraphOneCircle3.grpr";
-    GRAPH_ONE_CROSSROAD             = samples.__path__[0] + "\\graphs\\GraphOneCrossroad.grpr";
-    GRAPH_ONE_LINE                  = samples.__path__[0] + "\\graphs\\GraphOneLine.grpr";
-    GRAPH_TWO_CROSSROADS            = samples.__path__[0] + "\\graphs\\GraphTwoCrossroads.grpr";
-    GRAPH_FULL1                     = samples.__path__[0] + "\\graphs\\GraphFull1.grpr"
-    GRAPH_FULL2                     = samples.__path__[0] + "\\graphs\\GraphFull2.grpr"
-    GRAPH_SIMPLE1                   = samples.__path__[0] + "\\graphs\\GraphSimple1.grpr"
-    GRAPH_SIMPLE2                   = samples.__path__[0] + "\\graphs\\GraphSimple2.grpr"
-    GRAPH_SIMPLE3                   = samples.__path__[0] + "\\graphs\\GraphSimple3.grpr"
+    GRAPH_BROKEN_CIRCLE1            = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphBrokenCircle1.grpr";
+    GRAPH_BROKEN_CIRCLE2            = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphBrokenCircle2.grpr";
+    GRAPH_FIVE_POINTED_FRAME_STAR   = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphFivePointedFrameStar.grpr";
+    GRAPH_FIVE_POINTED_STAR         = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphFivePointedStar.grpr";
+    GRAPH_ONE_CIRCLE1               = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphOneCircle1.grpr";
+    GRAPH_ONE_CIRCLE2               = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphOneCircle2.grpr";
+    GRAPH_ONE_CIRCLE3               = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphOneCircle3.grpr";
+    GRAPH_ONE_CROSSROAD             = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphOneCrossroad.grpr";
+    GRAPH_ONE_LINE                  = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphOneLine.grpr";
+    GRAPH_TWO_CROSSROADS            = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphTwoCrossroads.grpr";
+    GRAPH_FULL1                     = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphFull1.grpr"
+    GRAPH_FULL2                     = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphFull2.grpr"
+    GRAPH_SIMPLE1                   = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphSimple1.grpr"
+    GRAPH_SIMPLE2                   = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphSimple2.grpr"
+    GRAPH_SIMPLE3                   = samples.__path__[0] + os.sep + "graphs" + os.sep + "GraphSimple3.grpr"
     
 class GRAPH_DSJC_SAMPLES:
-    DSJC_250_5                      = samples.__path__[0] + "\\graphs\\DSJC250.5.grpr";
-    DSJC_500_1                      = samples.__path__[0] + "\\graphs\\DSJC500.1.grpr";
-    DSJC_500_5                      = samples.__path__[0] + "\\graphs\\DSJC500.5.grpr";
-    DSJC_500_9                      = samples.__path__[0] + "\\graphs\\DSJC500.9.grpr";
-    DSJC_1000_1                     = samples.__path__[0] + "\\graphs\\DSJC1000.1.grpr";
-    DSJC_1000_5                     = samples.__path__[0] + "\\graphs\\DSJC1000.5.grpr";
-    DSJC_1000_9                     = samples.__path__[0] + "\\graphs\\DSJC1000.9.grpr";
+    DSJC_250_5                      = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC250.5.grpr";
+    DSJC_500_1                      = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC500.1.grpr";
+    DSJC_500_5                      = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC500.5.grpr";
+    DSJC_500_9                      = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC500.9.grpr";
+    DSJC_1000_1                     = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC1000.1.grpr";
+    DSJC_1000_5                     = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC1000.5.grpr";
+    DSJC_1000_9                     = samples.__path__[0] + os.sep + "graphs" + os.sep + "DSJC1000.9.grpr";
     
 class IMAGE_SIMPLE_SAMPLES:
-    IMAGE_THREE_OBJECT1             = samples.__path__[0] + "\\images\\ImageSimple1.png";
-    IMAGE_THREE_OBJECT2             = samples.__path__[0] + "\\images\\ImageSimple2.png";
-    IMAGE_THREE_OBJECT3             = samples.__path__[0] + "\\images\\ImageSimple3.png";
-    IMAGE_TWO_COLOR_SET             = samples.__path__[0] + "\\images\\ImageSimple4.png";
-    IMAGE_SIMPLE_BEACH              = samples.__path__[0] + "\\images\\ImageSimpleBeach.png";
+    IMAGE_THREE_OBJECT1             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple1.png";
+    IMAGE_THREE_OBJECT2             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple2.png";
+    IMAGE_THREE_OBJECT3             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple3.png";
+    IMAGE_TWO_COLOR_SET             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple4.png";
+    IMAGE_SIMPLE_BEACH              = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimpleBeach.png";
     
