497de56c028091110c0580733c873fb71950e628
annoviko <spb.andr@yandex.ru>
2014-08-20 22:41:04 +0400

    [clustering.syncnet] Segmentation examples (objects with colors) have been addded

--- clustering/syncnet/segmentation.py
+++ clustering/syncnet/segmentation.py
@@ -1,39 +1,104 @@
+from math import floor;
+
 from PIL import Image;
 
-from support import draw_image_segments, read_image;
+from support import draw_image_segments, read_image, draw_dynamics;
 
-from samples.definitions import IMAGE_SIMPLE_SAMPLES;
+from samples.definitions import IMAGE_SIMPLE_SAMPLES, IMAGE_MAP_SAMPLES;
 
 from clustering.syncnet import syncnet;
 
 from nnet import solve_type;
 
-def template_segmentation_image(source, show_dyn):    
+def template_segmentation_image(source, color_radius, object_radius, noise_size, show_dyn):    
     data = read_image(source);
 
-    network = syncnet(data, 50, ccore = True);
-    network.process(0.998, solve_type.FAST, False);
-    clusters = network.get_clusters();
+    network = syncnet(data, color_radius, ccore = True);
+    (t, dyn) = network.process(0.998, solve_type.FAST, show_dyn);
     
+    if (show_dyn is True):
+        draw_dynamics(t, dyn);
+    
+    clusters = network.get_clusters();    
     draw_image_segments(source, clusters);
     
+    if (object_radius is None):
+        return;
+    
+    # continue analysis
+    pointer_image = Image.open(source);
+    image_size = pointer_image.size;
+    
+    object_colored_clusters = [];
+    for cluster in clusters:
+        coordinates = [];
+        for index in cluster:
+            y = floor(index / image_size[0]);
+            x = index - y * image_size[0];
+            
+            coordinates.append([x, y]);
+        
+        print(coordinates);
+        
+        # perform clustering analysis of the colored objects
+        if (network is not None):
+            del network;
+            network = None;
+        
+        if (len(coordinates) < noise_size):
+            continue;
+        
+        network = syncnet(coordinates, object_radius, ccore = True);
+        network.process(0.998, solve_type.FAST, False);
+        
+        object_clusters = network.get_clusters();
+        
+        # decode it
+        real_description_clusters = [];
+        for object_cluster in object_clusters:
+            real_description = [];
+            for index_object in object_cluster:
+                real_description.append(cluster[index_object]);
+            
+            real_description_clusters.append(real_description);
+            
+            if (len(real_description) > noise_size):
+                object_colored_clusters.append(real_description);
+            
+        # draw_image_segments(source, [ cluster ]);
+        # draw_image_segments(source, real_description_clusters);
+    
+    draw_image_segments(source, object_colored_clusters);
+    
 def segmentation_image_simple1():
-    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT1, show_dyn = False);
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT1, 128, None, None, show_dyn = False);
     
 def segmentation_image_simple2():
-    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT2, show_dyn = False);  
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT2, 128, None, None, show_dyn = False);  
     
 def segmentation_image_simple3():
-    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT3, show_dyn = False);
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_OBJECT3, 128, None, None, show_dyn = False);
     
 def segmentation_image_simple4():
-    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_TWO_COLOR_SET, show_dyn = False);
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_TWO_COLOR_SET, 128, None, None, show_dyn = False);
     
+def segmentation_image_letters1():
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_LETTERS1, 128, 4, 10, show_dyn = False);
+
+def segmentation_image_letters2():
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_THREE_LETTERS2, 128, 4, 10, show_dyn = False);
+  
 def segmentation_image_beach():
-    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_SIMPLE_BEACH, show_dyn = False);
-    
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_SIMPLE_BEACH, 128, None, None, show_dyn = False);
+
+def segmentation_image_white_sea():
+    template_segmentation_image(IMAGE_MAP_SAMPLES.IMAGE_WHITE_SEA_SMALL, 128, None, None, show_dyn = False);
+
 segmentation_image_simple1();
 segmentation_image_simple2();
 segmentation_image_simple3();
 segmentation_image_simple4();
+segmentation_image_letters1();
+segmentation_image_letters2();
 segmentation_image_beach();
+# segmentation_image_white_sea();
--- samples/definitions.py
+++ samples/definitions.py
@@ -52,6 +52,11 @@ class IMAGE_SIMPLE_SAMPLES:
     IMAGE_THREE_OBJECT2             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple2.png";
     IMAGE_THREE_OBJECT3             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple3.png";
     IMAGE_TWO_COLOR_SET             = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple4.png";
+    IMAGE_THREE_LETTERS1            = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple5.png";
+    IMAGE_THREE_LETTERS2            = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimple6.png";
     IMAGE_SIMPLE_BEACH              = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimpleBeach.png";
     IMAGE_SIMPLE_DOUBLE_T           = samples.__path__[0] + os.sep + "images" + os.sep + "ImageSimpleDoubleT.png";
     
+class IMAGE_MAP_SAMPLES:
+    IMAGE_WHITE_SEA                 = samples.__path__[0] + os.sep + "images" + os.sep + "ImageWhiteSea.png";
+    IMAGE_WHITE_SEA_SMALL           = samples.__path__[0] + os.sep + "images" + os.sep + "ImageWhiteSeaSmall.png"
