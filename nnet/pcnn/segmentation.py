b0fd898594913e9bf7ee13337f3445d954294101
annoviko <spb.andr@yandex.ru>
2014-10-28 19:43:24 +0400

    [nnet.pcnn] Added new example for PCNN

--- nnet/pcnn/segmentation.py
+++ nnet/pcnn/segmentation.py
@@ -11,9 +11,17 @@ def template_segmentation_image(image, parameters, simulation_time, brightness):
     stimulus = read_image(image);
     stimulus = rgb2gray(stimulus);
     
-    for pixel_index in range(len(stimulus)):
-        if (stimulus[pixel_index] < brightness): stimulus[pixel_index] = 1;
-        else: stimulus[pixel_index] = 0;
+    if (brightness != None):
+        for pixel_index in range(len(stimulus)):
+            if (stimulus[pixel_index] < brightness): stimulus[pixel_index] = 1;
+            else: stimulus[pixel_index] = 0;
+    else:
+        maximum_stimulus = float(max(stimulus));
+        minimum_stimulus = float(min(stimulus));
+        delta = maximum_stimulus - minimum_stimulus;
+        
+        for pixel_index in range(len(stimulus)):
+            stimulus[pixel_index] = 1.0 - ((float(stimulus[pixel_index]) - minimum_stimulus) / delta);
     
     if (parameters is None):
         parameters = pcnn_parameters();
@@ -43,8 +51,24 @@ def segmentation_image_simple2():
 
 def segmentation_image_simple6():
     template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_SIMPLE06, None, 47, 128);
+    
+def segmentation_image_beach():
+    "Long processing"
+    parameters = pcnn_parameters();
+    
+    parameters.AF = 0.1;
+    parameters.AL = 0.1;
+    parameters.AT = 0.8;
+    parameters.VF = 1.0;
+    parameters.VL = 1.0;
+    parameters.VT = 30.0;
+    parameters.W = 1.0;
+    parameters.M = 1.0;
+        
+    template_segmentation_image(IMAGE_SIMPLE_SAMPLES.IMAGE_SIMPLE_BUILDING, parameters, 47, None);
 
 
 segmentation_image_simple1();
 segmentation_image_simple2();
 segmentation_image_simple6();
+segmentation_image_beach();
