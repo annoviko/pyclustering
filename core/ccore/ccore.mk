a5bcdf81c603e08df8a9d310f0848d53a6013cb6
annoviko <spb.andr@yandex.ru>
2014-11-12 00:09:35 +0400

    [core] Unit-test project for linux and linux adaptation

---
+++ core/ccore/ccore.mk
@@ -0,0 +1,24 @@
+# GCC should support C++11, for example, GCC 4.7.2
+# CCORE PyClustering project
+
+include makefile.include
+
+CC = g++
+CFLAGS = -std=gnu++0x -fPIC -c 
+LFLAGS = -std=gnu++0x -shared
+
+OBJECTS = $(SOURCES:.cpp=.o)
+
+EXECUTABLE = x64/linux/ccore.so
+
+ccore: $(EXECUTABLE)
+
+$(EXECUTABLE): $(OBJECTS)
+	$(CC) $(LFLAGS) $(OBJECTS) $(INCLUDES) -o $@
+
+.cpp.o:
+	$(CC) $(CFLAGS) $(INCLUDES) $< -o $@
+
+clean:
+	rm -rf ccore/*o ccore.so
+
--- core/ccore/utcore.mk
+++ core/ccore/utcore.mk
@@ -3,25 +3,11 @@
 
 CC = g++
 CFLAGS = -std=gnu++0x -fPIC -c -isystem tools/
-LFLAGS = -std=gnu++0x
+LFLAGS = -std=gnu++0x -pthread tools/gtest/lib/linux/libgtest.a
 
-LIBS += -pthread tools/gtest/lib/linux/libgtest.a
+include makefile.include
 
-INCLUDES = -I./ -Iutcore/ -Iccore/ -Itools/
-
-SOURCES += ccore/ccore.cpp
-SOURCES += ccore/cure.cpp
-SOURCES += ccore/dbscan.cpp
-SOURCES += ccore/hierarchical.cpp
-SOURCES += ccore/hsyncnet.cpp
-SOURCES += ccore/kdtree.cpp
-SOURCES += ccore/kmeans.cpp
-SOURCES += ccore/network.cpp
-SOURCES += ccore/rock.cpp
-SOURCES += ccore/support.cpp
-SOURCES += ccore/sync_network.cpp
-SOURCES += ccore/syncnet.cpp
-SOURCES += ccore/xmeans.cpp
+INCLUDES += -I./ -Iutcore/ -Itools/
 
 SOURCES += utcore/main.cpp
 
@@ -29,14 +15,13 @@ OBJECTS = $(SOURCES:.cpp=.o)
 
 EXECUTABLE = utcore/utcore
 
-utccore: $(EXECUTABLE)
+utcore: $(EXECUTABLE)
+	./utcore/utcore
 
 $(EXECUTABLE): $(OBJECTS)
-	$(CC) $(LFLAGS) $(OBJECTS) $(INCLUDES) -o $(EXECUTABLE)
+	$(CC) $(OBJECTS) $(LFLAGS) -o $(EXECUTABLE)
 
 .cpp.o:
-	$(CC) $(CFLAGS) $(LIBS) $(INCLUDES) $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDES) $< -o $@
 
-clean:
-	rm -rf ccore/*o ccore.so
 
