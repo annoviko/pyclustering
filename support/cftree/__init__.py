5a3d01bdfb6c3cde68e2f668a05aa97031600939
annoviko <spb.andr@yandex.ru>
2014-12-22 16:13:19 +0400

    [support.cftree] Fixed bugs with distance calculation

--- support/cftree/__init__.py
+++ support/cftree/__init__.py
@@ -183,7 +183,7 @@ class cfentry:
         
         linear_part_distance = sum(list_math_multiplication(linear_part_first, linear_part_second));
         
-        general_part_distance = 2.0 * (self.square_sum + entry.square_sum) - 2.0 * linear_part_distance;
+        general_part_distance = 2.0 * (self.number_points + entry.number_points) * (self.square_sum + entry.square_sum) - 2.0 * linear_part_distance;
         
         return (general_part_distance / ( (self.number_points + entry.number_points) * (self.number_points + entry.number_points - 1.0) )) ** 0.5;
     
@@ -196,16 +196,20 @@ class cfentry:
         "Return variance increase distance."
                 
         linear_part_12 = list_math_addition(self.linear_sum, entry.linear_sum);
-        variance_part_first = -2.0 * ( sum(list_math_multiplication(linear_part_12, linear_part_12)) ) / ( 1.0 / (self.number_points + entry.number_points) );
-        variance_part_second = sum(list_math_multiplication(linear_part_12, linear_part_12)) / (1.0 / (self.number_points + entry.number_points)**0.5 );
+        variance_part_first = (self.square_sum + entry.square_sum) - \
+            2.0 * sum(list_math_multiplication(linear_part_12, linear_part_12)) / (self.number_points + entry.number_points) + \
+            (self.number_points + entry.number_points) * sum(list_math_multiplication(linear_part_12, linear_part_12)) / (self.number_points + entry.number_points)**2.0;
+
         
         linear_part_11 = sum(list_math_multiplication(self.linear_sum, self.linear_sum));
-        variance_part_third = (2.0 / self.number_points) * linear_part_11 - (1.0 / self.number_points ** 2.0) * linear_part_11;
+        variance_part_second = -( self.square_sum - (2.0 * linear_part_11 / self.number_points) + (linear_part_11 / self.number_points) );
         
         linear_part_22 = sum(list_math_multiplication(entry.linear_sum, entry.linear_sum));
-        variance_part_fourth = (2.0 / entry.number_points) * linear_part_22 - (1.0 / entry.number_points ** 2.0) * linear_part_22;
+        variance_part_third = -( entry.square_sum - (2.0 / entry.number_points) * linear_part_22 + entry.number_points * (1.0 / entry.number_points ** 2.0) * linear_part_22 );
         
-        return (variance_part_first + variance_part_second + variance_part_third + variance_part_fourth);
+        print(variance_part_first, variance_part_second, variance_part_third);
+        
+        return (variance_part_first + variance_part_second + variance_part_third);
         
 
 
