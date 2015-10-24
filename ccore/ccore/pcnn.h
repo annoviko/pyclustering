969ed7508bf295bd176d6940ff194b6d89096176
annoviko <spb.andr@yandex.ru>
2015-10-25 02:20:44 +0400

    [ccore.pcnn] dynamic_oscillator_at() method has been added to obtain state of the specified oscillator at the specified step (requested by @madratman).

--- ccore/ccore/pcnn.h
+++ ccore/ccore/pcnn.h
@@ -80,6 +80,22 @@ public:
 	void allocate_spike_ensembles(ensemble_data<pcnn_ensemble> & spike_ensembles) const;
 
 	void allocate_time_signal(pcnn_time_signal & time_signal) const;
+
+public:
+    /***********************************************************************************************
+    *
+    * @brief   Returns dynamic state of oscillator of the pulse-coupled neural network at the
+    *          specified iteration step.
+    *
+    * @param[in] iteration (size_t): number of iteration at which oscillator state is required.
+    * @param[in] index_oscillator (size_t): index of oscillator whose state is required.
+    *
+    * @return (double) dynamic state of the oscillator at the specified iteration.
+    *
+    ***********************************************************************************************/
+    inline double dynamic_oscillator_at(const size_t iteration, const size_t index_oscillator) const { 
+        return dynamic_at(iteration).m_output[index_oscillator]; 
+    }
 };
 
 
