4c04b825ed26eec8156651a621da71ab3a1ce7be
annoviko <pyclustering@yandex.ru>
2015-11-13 12:30:49 +0100

    [ccore] removed automatically generated files for eclipse project.

--- ccore/utdebug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /home/andrei/workspace/pyclustering/ccore/tools/gtest/lib/linux/libgtest.a
-
-LIBS :=
-
