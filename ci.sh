0b6df6d5efe06567193b615649e3dabe6210dfd4
annoviko <spb.andr@yandex.ru>
2016-03-17 00:25:01 +0300

    [ci] code coverage (attempt 2).

--- ci.sh
+++ ci.sh
@@ -40,7 +40,7 @@ run_python_job() {
 	cd $library_folder
 
 	# run unit-tests with code coverage
-	coverage run pyclustering/ut/__init__.py
+	coverage run ut/__init__.py
 	coverage report --include="`python -c \"import os, pyclustering; print(os.path.dirname(pyclustering.__file__))\"`/*"
 	coveralls
 }
