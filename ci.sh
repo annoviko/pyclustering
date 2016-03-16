098c7daed3cab1de403e4d0722b4993dca4c5bbf
annoviko <spb.andr@yandex.ru>
2016-03-16 23:50:13 +0300

    [ci] coverage reporting

--- ci.sh
+++ ci.sh
@@ -38,7 +38,7 @@ run_python_job() {
 
 	# set current folder where installed library is located
 	library_folder="`python -c \"import os, pyclustering; print(os.path.dirname(pyclustering.__file__))\"`/*";
-	cd library_folder
+	cd $library_folder
 
 	# run unit-tests with code coverage
 	coverage run pyclustering/ut/__init__.py
