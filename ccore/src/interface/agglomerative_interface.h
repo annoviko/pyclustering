c026e407beb1183801ffff3bf5097bfd9f77e77d
annoviko <spb.andr@yandex.ru>
2016-05-17 01:05:57 +0300

    [ccore] Commit forgotten file.

---
+++ ccore/src/interface/agglomerative_interface.h
@@ -0,0 +1,47 @@
+/**
+*
+* Copyright (C) 2014-2016    Andrei Novikov (pyclustering@yandex.ru)
+*
+* GNU_PUBLIC_LICENSE
+*   pyclustering is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   pyclustering is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef SRC_INTERFACE_AGGLOMERATIVE_INTERFACE_H_
+#define SRC_INTERFACE_AGGLOMERATIVE_INTERFACE_H_
+
+
+#include "interface/pyclustering_package.hpp"
+
+#include "definitions.hpp"
+#include "utils.hpp"
+
+
+/**
+ *
+ * @brief   Clustering algorithm Agglomerative returns allocated clusters.
+ * @details Caller should destroy returned result in 'pyclustering_package'.
+ *
+ * @param[in] p_sample: input data for clustering.
+ * @param[in] p_number_clusters: amount of clusters that should be allocated.
+ * @param[in] p_link: type of links for merging clusters.
+ *
+ * @return  Returns result of clustering - array of allocated clusters. The last cluster in the
+ *          array is noise.
+ *
+ */
+extern "C" DECLARATION pyclustering_package * agglomerative_algorithm(const data_representation * const p_sample, const size_t p_number_clusters, const size_t p_link);
+
+
+#endif
