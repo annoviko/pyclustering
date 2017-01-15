31b63e70778dce94eee590e7bca9fa11da1c5fd7
annoviko <spb.andr@yandex.ru>
2017-01-15 21:14:09 +0300

    [ccore::cluster_analysis::ordering_analyser] Propagate correction from pyclustering OPTICS and added unit-tests for analyser.

--- ccore/src/cluster/ordering_analyser.cpp
+++ ccore/src/cluster/ordering_analyser.cpp
@@ -33,77 +33,84 @@ ordering_analyser::ordering_analyser(const ordering_ptr & p_ordering) : m_orderi
 
 
 double ordering_analyser::calculate_connvectivity_radius(const std::size_t p_amount_clusters, const std::size_t p_maximum_iterations) const {
-	const double maximum_distance = *std::max_element(m_ordering->cbegin(), m_ordering->cend());
+    const double maximum_distance = *std::max_element(m_ordering->cbegin(), m_ordering->cend());
 
-	double upper_distance = maximum_distance;
-	double lower_distance = 0.0;
+    double upper_distance = maximum_distance;
+    double lower_distance = 0.0;
 
-	double radius = -1.0;
-	
-	if (extract_cluster_amount(maximum_distance) <= p_amount_clusters) {
-		for(std::size_t i = 0; i < p_maximum_iterations; i++) {
-			radius = (lower_distance + upper_distance) / 2.0;
+    double radius = -1.0;
+    double result = -1.0;
 
-			std::size_t amount = extract_cluster_amount(radius);
-			if ( (amount == p_amount_clusters) && (amount == std::numeric_limits<std::size_t>::max()) ) {
-				break;
-			}
-			else if (amount > p_amount_clusters) {
-				lower_distance = radius;
-			}
-			else if (amount < p_amount_clusters) {
-				upper_distance = radius;
-			}
-		}
-	}
+    if (extract_cluster_amount(maximum_distance) <= p_amount_clusters) {
+        for(std::size_t i = 0; i < p_maximum_iterations; i++) {
+            radius = (lower_distance + upper_distance) / 2.0;
 
-	return radius;
+            std::size_t amount = extract_cluster_amount(radius);
+            if (amount == p_amount_clusters) {
+                result = radius;
+                break;
+            }
+            else if (amount == 0) {
+                break;
+            }
+            else if (amount > p_amount_clusters) {
+                lower_distance = radius;
+            }
+            else if (amount < p_amount_clusters) {
+                upper_distance = radius;
+            }
+        }
+    }
+
+    return result;
 }
 
 
 std::size_t ordering_analyser::extract_cluster_amount(const double p_radius) const {
-	std::size_t amount_clusters = 1;
+    std::size_t amount_clusters = 1;
 
-	bool cluster_start = false;
-	bool cluster_pick = false;
-	bool total_similariry = true;
+    bool cluster_start = false;
+    bool cluster_pick = false;
+    bool total_similariry = true;
 
-	double previous_cluster_distance = 0.0;
-	double previous_distance = -1.0;
+    double previous_cluster_distance = 0.0;
+    double previous_distance = -1.0;
 
-	for (auto & distance : *m_ordering) {
-		if (distance >= p_radius) {
-			if (!cluster_start) {
-				cluster_start = true;
-				amount_clusters++;
-			}
-			else {
-				if ( (distance < previous_cluster_distance) && (!cluster_pick) ) {
-					cluster_pick = true;
-				}
-				else if ( (distance > previous_cluster_distance) && (cluster_pick) ) {
-					cluster_pick = false;
-					amount_clusters += 1;
-				}
-			}
+    for (auto & distance : *m_ordering) {
+        if (distance >= p_radius) {
+            if (!cluster_start) {
+                cluster_start = true;
+                amount_clusters++;
+            }
+            else {
+                if ( (distance < previous_cluster_distance) && (!cluster_pick) ) {
+                    cluster_pick = true;
+                }
+                else if ( (distance > previous_cluster_distance) && (cluster_pick) ) {
+                    cluster_pick = false;
+                    amount_clusters += 1;
+                }
+            }
 
-			previous_cluster_distance = distance;
-		}
-		else {
-			cluster_start = false;
-			cluster_pick = false;
-		}
+            previous_cluster_distance = distance;
+        }
+        else {
+            cluster_start = false;
+            cluster_pick = false;
+        }
 
-		if ( (previous_distance >= 0) && (previous_distance != distance) ) {
-			total_similariry = false;
-		}
-	}
+        if ( (previous_distance >= 0) && (previous_distance != distance) ) {
+            total_similariry = false;
+        }
 
-	if ( (total_similariry) && (previous_distance > p_radius) ) {
-		amount_clusters = 0;
-	}
+        previous_distance = distance;
+    }
 
-	return amount_clusters;
+    if ( (total_similariry) && (previous_distance > p_radius) ) {
+        amount_clusters = 0;
+    }
+
+    return amount_clusters;
 }
 
 
---
+++ ccore/tst/utest-ordering_analyser.hpp
@@ -0,0 +1,54 @@
+/**
+*
+* Copyright (C) 2014-2017    Andrei Novikov (pyclustering@yandex.ru)
+*
+* GNU_PUBLIC_LICENSE
+*   pyclustering is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   pyclustering is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#pragma once
+
+
+#include "gtest/gtest.h"
+
+#include "cluster/ordering_analyser.hpp"
+
+#include "samples.hpp"
+#include "utest-cluster.hpp"
+
+
+TEST(utest_ordering, cluster_allocation_identical_ordering) {
+    ordering_ptr cluster_ordering = std::shared_ptr<ordering>(new ordering({5.0, 5.0, 5.0, 5.0, 5.0, 5.0}));
+    ordering_analyser analyser(cluster_ordering);
+
+    EXPECT_EQ(1, analyser.extract_cluster_amount(6.5));
+    EXPECT_EQ(0, analyser.extract_cluster_amount(4.5));
+}
+
+
+TEST(utest_ordering, impossible_calculate_radius_identical_ordering) {
+    ordering_ptr cluster_ordering = std::shared_ptr<ordering>(new ordering({5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0}));
+    ordering_analyser analyser(cluster_ordering);
+
+    EXPECT_TRUE(analyser.calculate_connvectivity_radius(2) < 0);
+}
+
+
+TEST(utest_ordering, impossible_calculate_radius_geterogeneous_ordering) {
+    ordering_ptr cluster_ordering = std::shared_ptr<ordering>(new ordering({5.0, 5.0, 5.0, 5.0, 6.0, 8.0, 6.0, 5.0, 5.0, 5.0}));
+    ordering_analyser analyser(cluster_ordering);
+
+    EXPECT_TRUE(analyser.calculate_connvectivity_radius(3) < 0);
+}
