d5e718a3d3664b7130173a8784bb86fe49e6e2c5
annoviko <spb.andr@yandex.ru>
2017-02-23 21:47:47 +0300

    [ccore] Remove usage makefile.include from ccore.mk

--- ccore/ccore.mk
+++ ccore/ccore.mk
@@ -1,29 +1,44 @@
-# CCORE PyClustering library project
-
-include makefile.include
-
-CC = g++
+# Tools
+CC = g++ -c
 LD = g++
+RM = rm -rf
+MKDIR = mkdir -p
+
 
 # Toolchain arguments.
-CFLAGS = -O3 -MMD -MP -std=c++1y -fPIC -c 
-LFLAGS = -O3 -static-libstdc++ -shared
+CFLAGS = -O3 -MMD -MP -std=c++1y -fPIC
+LFLAGS = -static-libstdc++ -shared
+
 
 # Project sources
+MODULES = cluster container differential interface nnet tsp
+
+SOURCES_DIRECTORY = src
+SOURCES_DIRECTORIES = $(addprefix $(SOURCES_DIRECTORY)/, $(MODULES))
+SOURCES = $(foreach SUBDIR, $(SOURCES_DIRECTORIES), $(wildcard $(SUBDIR)/*.cpp))
+
+INCLUDES = -I$(SOURCES_DIRECTORY)
+
+
+# Project objects
 OBJECTS = $(SOURCES:.cpp=.o)
+
 
 # The dependency file names
 DEPS = $(OBJECTS:.o=.d)
+
 
 # Executable library file
 EXECUTABLE = ../pyclustering/core/x64/linux/ccore.so
 
 
+.PHONY: ccore
 ccore: $(EXECUTABLE)
 
 
+.PHONY:
 clean:
-	rm -rf ccore/*o ccore.so
+	$(RM) ccore/*o ccore.so
 
 
 $(EXECUTABLE): $(OBJECTS)
