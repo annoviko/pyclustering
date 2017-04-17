21198949fb52ecf570959ac53d4e5126bfb5d3a0
annoviko <spb.andr@yandex.ru>
2017-04-17 13:20:39 +0300

    [pyclustering.utils] Added forgotten file.

---
+++ pyclustering/utils/color.py
@@ -0,0 +1,39 @@
+"""!
+
+@brief Colors used by pyclustering library for visualization.
+
+@authors Andrei Novikov (pyclustering@yandex.ru)
+@date 2014-2017
+@copyright GNU Public License
+
+@cond GNU_PUBLIC_LICENSE
+    PyClustering is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    PyClustering is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@endcond
+
+"""
+
+
+class color:
+    """!
+    @brief Consists titles of colors that are used by pyclustering for visualization.
+    
+    """
+    
+    ## List of color titles that are used by pyclustering for visualization.
+    TITLES = [  'red', 'blue', 'darkgreen', 'brown', 'violet', 
+                'deepskyblue', 'darkgrey', 'lightsalmon', 'deeppink', 'yellow',
+                'black', 'mediumspringgreen', 'orange', 'darkviolet', 'darkblue',
+                'silver', 'lime', 'pink', 'gold', 'bisque',
+                'dimgray', 'firebrick', 'darksalmon', 'chartreuse', 'skyblue',
+                'purple', 'fuchsia', 'palegoldenrod', 'coral', 'hotpink'                ];
