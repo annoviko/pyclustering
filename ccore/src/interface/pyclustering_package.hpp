d254c6e6e5600b69dd8ab473d0e91e2c4b8743ac
annoviko <spb.andr@yandex.ru>
2017-07-04 16:04:04 +0300

    [ccore] Build correction for linux (gcc).

--- ccore/src/interface/pyclustering_package.hpp
+++ ccore/src/interface/pyclustering_package.hpp
@@ -112,7 +112,7 @@ pyclustering_package * create_package(const TypeContainer * const data) {
 
 template <class TypeObject>
 pyclustering_package * create_package(const std::vector< std::vector<TypeObject> * > * const data) {
-   pyclustering_package * package = new pyclustering_package((unsigned int) pyclustering_type_data::PYCLUSTERING_TYPE_LIST);
+   pyclustering_package * package = new pyclustering_package(pyclustering_type_data::PYCLUSTERING_TYPE_LIST);
 
    package->size = data->size();
    package->data = new pyclustering_package * [package->size];
