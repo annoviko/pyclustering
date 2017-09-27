37a55bef036dcceea1ad2825533707bff89addcc
annoviko <spb.andr@yandex.ru>
2017-09-27 18:29:13 +0300

    #349: Stages/jobs in travis-ci.

--- ci/travis-ci-deploy.sh
+++ ci/travis-ci-deploy.sh
@@ -4,14 +4,23 @@ run_deploy_job() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     
-    ls -la
-    git status
+    echo "[DEPLOY]: Switch to branch '$TRAVIS_BRANCH'"
     git checkout $TRAVIS_BRANCH
     
+    echo "[DEPLOY]: Pull changes to make push clean"
+    git pull
+    
+    echo "[DEPLOY]: Display status and changes"
+    git status
+    git diff
+    
+    echo "[DEPLOY]: Prepare changes and commit them"
     mkdir pyclustering/core/x64/linux
     echo "linux ccore x64 build version: '$TRAVIS_BUILD_NUMBER'" > pyclustering/core/x64/linux/.linux.info
     git add pyclustering/core/x64/linux/.linux.info
+    git status
     
+    echo "[DEPLOY]: Push changes to github repository"
     git commit . -m "[travis-ci][ci skip] push new ccore version '$TRAVIS_BUILD_NUMBER'"
     git push
 }
