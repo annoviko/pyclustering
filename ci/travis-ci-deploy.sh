cfa37c86889337e0d6cb8090a8b2b8e032fc0eae
annoviko <spb.andr@yandex.ru>
2017-10-06 16:13:00 +0300

    [ci] CI script is updated.

--- ci/travis-ci-deploy.sh
+++ ci/travis-ci-deploy.sh
@@ -1,32 +1,34 @@
 run_deploy_job() {
     echo "[DEPLOY]: Deploy (upload linux binary file to github)"
 
-    git config credential.helper "store --file=.git/credentials"; 
-    echo "https://${GITHUB_OAUTH_TOKEN}:@github.com" > .git/credentials 2> /dev/null
-
-    git config --global user.email "travis@travis-ci.org"
+    git config --global user.email "pyclustering@yandex.ru"
     git config --global user.name "Travis-CI"
+
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
 
     echo "[DEPLOY]: Switch to branch '$TRAVIS_BRANCH'"
     git checkout $TRAVIS_BRANCH
 
+
     echo "[DEPLOY]: Pull changes to make push clean"
     git pull
 
-    echo "[DEPLOY]: Display status and changes"
-    git status
-    git diff
 
     echo "[DEPLOY]: Prepare changes and commit them"
     mkdir pyclustering/core/x64/linux
     echo "linux ccore x64 build version: '$TRAVIS_BUILD_NUMBER'" > pyclustering/core/x64/linux/.linux.info
     git add pyclustering/core/x64/linux/.linux.info
+
+
+    echo "[DEPLOY]: Display status and changes"
     git status
 
 
     echo "[DEPLOY]: Push changes to github repository"
     git commit . -m "[travis-ci][ci skip] push new ccore version '$TRAVIS_BUILD_NUMBER'"
-    git push --quiet
+    git push
 }
 
 
