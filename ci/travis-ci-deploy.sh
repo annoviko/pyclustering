32d4792a77f75429bfffb5e7cd55012513ec7b4b
annoviko <spb.andr@yandex.ru>
2017-10-10 11:37:19 +0300

    [ci] Deploy linux build.

--- ci/travis-ci-deploy.sh
+++ ci/travis-ci-deploy.sh
@@ -15,10 +15,15 @@ run_deploy_job() {
     git pull
 
 
-    echo "[DEPLOY]: Prepare changes and commit them"
+    echo "[DEPLOY]: Prepare binary folder"
     mkdir pyclustering/core/x64/linux
+
+    download_binary
+
+    echo "[DEPLOY]: Add changes for commit"
     echo "linux ccore x64 build version: '$TRAVIS_BUILD_NUMBER'" > pyclustering/core/x64/linux/.linux.info
     git add pyclustering/core/x64/linux/.linux.info
+    git add pyclustering/core/x64/linux/ccore.so
 
 
     echo "[DEPLOY]: Display status and changes"
@@ -29,5 +34,24 @@ run_deploy_job() {
     git commit . -m "[travis-ci][ci skip] push new ccore version '$TRAVIS_BUILD_NUMBER'"
     git push
 }
+
+
+download_binary() {
+    echo "[DEPLOY]: Download binary file"
+
+    # Obtain link for download
+    BUILD_FOLDER=linux
+    BINARY_FOLDER=$TRAVIS_BUILD_NUMBER
+    BINARY_FILENAME=ccore.so
+    
+    DOWNLOAD_LINK=`curl -s -H "Authorization: OAuth $YANDEX_DISK_TOKEN" -X GET https://cloud-api.yandex.net:443/v1/disk/resources/download?path=$BUILD_FOLDER%2F$BINARY_FOLDER%2F$BINARY_FILENAME |\
+        python3 -c "import sys, json; print(json.load(sys.stdin)['href'])"`
+
+    curl -H "Authorization: OAuth $YANDEX_DISK_TOKEN" -o $DOWNLOAD_LINK
+
+    # Copy to binary folder
+    mv ccore.so pyclustering/core/x64/linux/ccore.so
+}
+
 
 run_deploy_job
