82f75839bcbf4c2f74c71e51bbedbf898b424a12
annoviko <spb.andr@yandex.ru>
2017-11-02 19:22:50 +0300

    [.][publish] Trigger deploy/publish and added additional print to deploy.

--- ci/travis-ci-deploy.sh
+++ ci/travis-ci-deploy.sh
@@ -48,6 +48,8 @@ download_binary() {
     BINARY_FOLDER=$TRAVIS_BUILD_NUMBER
     BINARY_FILEPATH=$TRAVIS_BRANCH%2F$BUILD_FOLDER%2F$BINARY_FOLDER%2Fccore.so
 
+    echo "[DEPLOY]: Download binary using binary path: '$BINARY_FILEPATH'"
+
     DOWNLOAD_LINK=`curl -s -H "Authorization: OAuth $YANDEX_DISK_TOKEN" -X GET https://cloud-api.yandex.net:443/v1/disk/resources/download?path=$BINARY_FILEPATH |\
         python3 -c "import sys, json; print(json.load(sys.stdin)['href'])"`
 
