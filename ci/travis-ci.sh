2f3be5d227fb76f7a9dbe5c9fa7ab3908d50ec7f
annoviko <spb.andr@yandex.ru>
2017-12-06 11:04:43 +0300

    #253: Try to build x86.

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -165,11 +165,11 @@ run_integration_test_job() {
     PLATFORM_TARGET=$1
 
     # install requirements for the job
+    install_miniconda $PLATFORM_TARGET
+
     sudo apt-get install -qq g++-5
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 50
     sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 50
-
-    install_miniconda $PLATFORM_TARGET
 
     # build ccore library
     build_ccore $PLATFORM_TARGET
@@ -275,7 +275,7 @@ install_miniconda() {
     fi
     
     print_info "Installing Miniconda."
-    bash miniconda.sh -b -p $HOME/miniconda
+    sudo bash miniconda.sh -b -p $HOME/miniconda
 
     export PATH="$HOME/miniconda/bin:$PATH"
     hash -r
