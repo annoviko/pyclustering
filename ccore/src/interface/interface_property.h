bc26876eff612591c8e5f3a2f56c35a5a82e95c7
annoviko <spb.andr@yandex.ru>
2018-01-30 17:12:12 +0300

    #393: Added forgitten files.

---
+++ ccore/src/interface/interface_property.h
@@ -0,0 +1,34 @@
+/**
+*
+* Copyright (C) 2014-2018    Andrei Novikov (pyclustering@yandex.ru)
+*
+* GNU_PUBLIC_LICENSE
+*   pyclustering is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   pyclustering is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#pragma once
+
+
+#include "definitions.hpp"
+
+
+/**
+ *
+ * @brief   Returns text description of the library
+ *
+ * @returns Returns const char pointer to text library description.
+ *
+ */
+extern "C" DECLARATION void * get_interface_description(void);
