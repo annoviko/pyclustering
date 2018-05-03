8e21ad845d9fcdeb08a17bb7fe2a453e2ee3c6c3
annoviko <spb.andr@yandex.ru>
2018-05-03 17:30:34 +0300

    #426: Citation unification.

---
+++ pyclustering/cluster/bang.py
@@ -0,0 +1,24 @@
+"""!
+
+@brief Cluster analysis algorithm: BANG @cite 10.1007/BFb0033313.
+
+@authors Andrei Novikov (pyclustering@yandex.ru)
+@date 2014-2018
+@copyright GNU Public License
+
+@cond GNU_PUBLIC_LICENSE
+    PyClustering is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyClustering is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@endcond
+
+"""
