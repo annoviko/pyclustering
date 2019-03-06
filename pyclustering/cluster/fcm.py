e00534f846fa7fe008c16887a258495e9fa823e8
annoviko <spb.andr@yandex.ru>
2019-03-06 21:07:20 +0300

    #386: File for implementation of FCM it *.bib for it.

---
+++ pyclustering/cluster/fcm.py
@@ -0,0 +1,30 @@
+"""!
+
+@brief Cluster analysis algorithm: Fuzzy C-Means
+@details Implementation based on paper @cite book::pattern_recognition_with_fuzzy.
+
+@authors Andrei Novikov (pyclustering@yandex.ru)
+@date 2014-2019
+@copyright GNU Public License
+
+@cond GNU_PUBLIC_LICENSE
+    PyClustering is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyClustering is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@endcond
+
+"""
+
+
+class fcm:
+    def __init__(self, data, initial_centers, **kwargs):
+        pass
