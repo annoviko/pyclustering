6cf5df188a7b2c535c7037b8d6dd6d0fa3d68492
annoviko <spb.andr@yandex.ru>
2020-10-30 23:39:04 +0100

    #603: cmake generates makefile with targets.

--- ccore/include/pyclustering/definitions.hpp
+++ ccore/include/pyclustering/definitions.hpp
@@ -40,10 +40,10 @@
 #elif defined (WIN32) || (_WIN32) || (_WIN64)
     #if defined(EXPORT_PYCLUSTERING_INTERFACE)
         #define DECLARATION __declspec(dllexport)
-    #elif defined(BUILDING_UT_PROJECT)
-        #define DECLARATION
-    #else
+    #elif defined(IMPORT_PYCLUSTERING_INTERFACE)
         #define DECLARATION __declspec(dllimport)
+    #else
+        #define DECLARATION
     #endif
 #else
     #error Unsupported platform
